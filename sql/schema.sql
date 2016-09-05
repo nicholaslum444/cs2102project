c67c326c0ffd5bc74952680e097e559f44c8dc8c
Nicholas Lum <nicholaslum444@hotmail.com>
2016-09-06 00:53:33 +0800

    added schema and readme
    Signed-off-by: Nicholas Lum <nicholaslum444@hotmail.com>

---
+++ sql/schema.sql
@@ -0,0 +1,75 @@
+DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS task;
+DROP TABLE IF EXISTS submit;
+DROP TABLE IF EXISTS offer;
+DROP TABLE IF EXISTS contract;
+
+CREATE TABLE IF NOT EXISTS admin (
+    id SERIAL NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    password_hash VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS account (
+    id SERIAL NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    password_hash VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS task (
+    id SERIAL NOT NULL,
+    title VARCHAR(256) NOT NULL,
+    description TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS submit (
+    id SERIAL NOT NULL,
+    creator_id INTEGER NOT NULL,
+    task_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (creator_id, task_id),
+    FOREIGN KEY (creator_id)
+        REFERENCES account(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (task_id)
+        REFERENCES task(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS offer (
+    id SERIAL NOT NULL,
+    offerer_id INTEGER NOT NULL,
+    task_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (offerer_id, task_id),
+    FOREIGN KEY (offerer_id)
+        REFERENCES account(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (task_id)
+        REFERENCES task(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS contract (
+    id SERIAL NOT NULL,
+    contractor_id INTEGER NOT NULL,
+    task_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (contractor_id, task_id),
+    FOREIGN KEY (contractor_id)
+        REFERENCES account(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (task_id)
+        REFERENCES task(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
---
+++ sql/sql_readme.md
@@ -0,0 +1,10 @@
+HOW TO USE SCHEMA.SQL
+---
+1. Visit localhost/phppgadmin and log in to pgadmin
+2. Top left corner of the page, click on "SQL"
+3. SQL input window will pop up
+4. Top left corner of the window, select database "nusmaids"
+5. If "nusmaids" is not present, create the database first with this SQL: `CREATE DATABASE nusmaids;`
+6. Open `schema.sql` in text editor
+7. Copy and paste all text into the SQL input window and execute
+8. Check that tables are created in "nusmaids" database
