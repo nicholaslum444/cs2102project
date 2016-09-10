e50616fceb6af90b2cf60053a34a5040b7ab152d
jiawei <jaewei.j.w@gmail.com>
2016-09-10 15:51:11 +0800

    Updated DB schema. DB seeds (only contains insert)

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,18 +1,9 @@
+DROP TABLE IF EXISTS person CASCADE;
+DROP TABLE IF EXISTS task CASCADE;
 DROP TABLE IF EXISTS contract;
-DROP TABLE IF EXISTS offer;
-DROP TABLE IF EXISTS submit;
-DROP TABLE IF EXISTS task;
-DROP TABLE IF EXISTS account;
-DROP TABLE IF EXISTS admin;
+DROP TYPE IF EXISTS status_type;
 
-CREATE TABLE IF NOT EXISTS admin (
-    id SERIAL NOT NULL,
-    email VARCHAR(256) NOT NULL,
-    password_hash VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS account (
+CREATE TABLE IF NOT EXISTS person (
     id SERIAL NOT NULL,
     email VARCHAR(256) NOT NULL,
     password_hash VARCHAR(256) NOT NULL,
@@ -21,55 +12,39 @@ CREATE TABLE IF NOT EXISTS account (
 
 CREATE TABLE IF NOT EXISTS task (
     id SERIAL NOT NULL,
-    title VARCHAR(256) NOT NULL,
+    title VARCHAR(512) NOT NULL,
     description TEXT,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS submit (
-    id SERIAL NOT NULL,
+    start_datetime TIMESTAMP NOT NULL,
+    end_datetime TIMESTAMP NOT NULL,
     creator_id INTEGER NOT NULL,
-    task_id INTEGER NOT NULL,
+    workers_needed INTEGER CHECK (workers_needed > 0),
+    price INTEGER CHECK (price > 0),
+    created_datetime TIMESTAMP NOT NULL,
+    last_updated_datetime TIMESTAMP NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE (creator_id, task_id),
-    FOREIGN KEY (creator_id)
-        REFERENCES account(id)
+    FOREIGN KEY (creator_id) REFERENCES person(id) 
         ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    FOREIGN KEY (task_id)
-        REFERENCES task(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+        ON DELETE RESTRICT
 );
 
-CREATE TABLE IF NOT EXISTS offer (
-    id SERIAL NOT NULL,
-    offerer_id INTEGER NOT NULL,
-    task_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (offerer_id, task_id),
-    FOREIGN KEY (offerer_id)
-        REFERENCES account(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    FOREIGN KEY (task_id)
-        REFERENCES task(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+CREATE TYPE status_type AS ENUM (
+    'pending', 
+    'ongoing', 
+    'completed', 
+    'cancelled'
 );
 
 CREATE TABLE IF NOT EXISTS contract (
     id SERIAL NOT NULL,
-    contractor_id INTEGER NOT NULL,
+    creator_id INTEGER NOT NULL,
     task_id INTEGER NOT NULL,
+    start_datetime TIMESTAMP NOT NULL,
+    end_datetime TIMESTAMP NOT NULL,
+    accepted_conditions BOOLEAN NOT NULL,
+    status status_type NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE (contractor_id, task_id),
-    FOREIGN KEY (contractor_id)
-        REFERENCES account(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    FOREIGN KEY (task_id)
-        REFERENCES task(id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+    FOREIGN KEY (creator_id) REFERENCES person(id)
+        ON DELETE RESTRICT,
+    FOREIGN KEY (task_id) REFERENCES task(id)
+        ON DELETE RESTRICT
 );
--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,17 +1,67 @@
+-------------------- PERSONS --------------------
+INSERT INTO person (email, password_hash) VALUES
+    ('Tampopo@live.com', '1985-02-10Comedy'),
+    ('HG120@mail.com', 'The Dinner GameComedy');
+
 -------------------- TASKS --------------------
 INSERT INTO task (
     title, 
-    description
+    description,
+    start_datetime,
+    end_datetime,
+    creator_id,
+    workers_needed,
+    price,
+    created_datetime,
+    last_updated_datetime
 ) VALUES (
     'Clean Car', 
-    'Clean my car for good karma. Satisfaction guaranteed.'
+    'Clean my car for good karma. Satisfaction guaranteed.',
+    now(),
+    now(),
+    1,
+    1,
+    10,
+    now(),
+    now()
 );
 
 INSERT INTO task (
     title, 
-    description
+    description,
+    start_datetime,
+    end_datetime,
+    creator_id,
+    workers_needed,
+    price,
+    created_datetime,
+    last_updated_datetime
 ) VALUES (
     'Sweep Floor', 
-    'When your CAP hits the floor, sweep it up together with your future.'
+    'When your CAP hits the floor, sweep it up together with your future.',
+    now(),
+    now(),
+    1,
+    1,
+    10,
+    now(),
+    now()
+);
+
+-------------------- CONTRACTS --------------------
+INSERT INTO contract (
+	creator_id,
+	task_id,
+	start_datetime,
+	end_datetime,
+	accepted_conditions,
+	status
+) VALUES (
+	1,
+	1,
+	now(),
+	now(),
+	TRUE,
+	'pending'
 );
 -------------------- ENDTASKS -----------------
