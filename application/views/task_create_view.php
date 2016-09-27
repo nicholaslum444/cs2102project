e037cc0ca27d84847b3a151517cc394ff71092a3
DESKTOP-50V5P2J\admin <samson.g@live.com>
2016-09-27 22:48:28 +0800

    updated css and task create view

--- application/views/task_create_view.php
+++ application/views/task_create_view.php
@@ -1,25 +1,23 @@
-<div>
-	<h1><?php echo $header ?></h1>
-	<hr>
-	<?php echo validation_errors(); ?>
+<div id="regHeader"><br/><br/><br/>
+    <div id="regBody"><br/>
+		<h1><?php echo $header ?></h1>
+		<span class="errorValidation"><?php echo validation_errors(); ?></span>
 
-	<?php echo form_open('task/validate'); ?>
+		<?php echo form_open('task/validate'); ?>
+	    <input type="input" class="form-control" name="title" placeholder="Title of your task"/><br/>
 
-	    <label for="title">Title</label><br />
-	    <input type="input" name="title" /><br />
+	    <textarea type="input" class="textarea-control" name="description" placeholder="Description of your task"/></textarea><br/>
 
-	    <label for="description">Describe your task!</label><br />
-	    <textarea type="input" name="description"></textarea><br />
+	    <label for="text">Enter your start date and time</label><br/>
+	    <input type="date" class="form-control" name="start_date" ><br/>
+	    <input type="time" class="form-control" name="start_time"><br/>
 
-	    <label for="text">Start On</label><br />
-	    <input type="date" name="start_date">
-	    <input type="time" name="start_time"><br />
+	    <label for="text">Enter your end date and time</label><br/>
+	    <input type="date" class="form-control" name="end_date"><br/>
+	    <input type="time" class="form-control" name="end_time"><br/>
 
-	    <label for="text">End By</label><br />
-	    <input type="date" name="end_date">
-	    <input type="time" name="end_time"><br />
-
-	    <input type="submit" name="submit" value="Create Task" />
-
-	</form>
+	    <br/>
+	    <input type="submit" class="alignSignUpButton" name="submit" value="Create Task" />
+	    <?php echo form_close('<br>'); ?>
+	</div>
 </div>
